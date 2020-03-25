8a2fb202e46f36ffd8811bbc950b1a90d67aac4b
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-25 23:55:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -=1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
